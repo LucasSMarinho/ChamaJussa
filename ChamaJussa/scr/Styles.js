34fef6bd0626d41cabcd3c631a22ecb040c2cd13
LUCAS SOUZA MARINHO <45193262813@info.local>
2026-08-18 08:37:53 -0300

    inicialização do projeto ChamaJussa em ReactNative

---
+++ ChamaJussa/scr/Styles.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F3F4F6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginBox: {
+    backgroundColor: "white",
+    height: 328,
+    width: "80%",
+    borderRadius: 24,
+
+    
+    paddingTop: 30,
+    padding: 10,
+  },
+  title: {
+   fontFamily: "Montserrat_700Bold",
+   fontSize: 21,
+  },
+  subTitle: {
+   fontFamily: "Montserrat_300Light",
+   fontSize: 13,
+   fontWeight: 400,
+   color: "#868585"
+  },
+  titleBox: {
+    alignItems: "center",
+    gap: 10
+  },
+  inputBox: {
+    
+  }     
+});
